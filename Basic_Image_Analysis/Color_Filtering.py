06916ceba5fe4541189e81787e89c768a180faf9
SanchitGupta001 <sanchit.gupta011@gmail.com>
2016-11-01 00:52:50 +0530

    Modified Color_Filtering.py

--- Basic_Image_Analysis/Color_Filtering.py
+++ Basic_Image_Analysis/Color_Filtering.py
@@ -15,11 +15,27 @@ while True:
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask=mask)
     
-    cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
-    cv2.imshow('res', res)
+    # Blurring Techniques:
+    kernel = np.ones((15,15), np.float32)/(15*15)
+    smoothed = cv2.filter2D(res, -1, kernel)
     
-    k = cv2.waitKey(0) & 0xFF
+    # Using Gaussian Blur technique
+    blur = cv2.GaussianBlur(res, (15,15), 0)
+    # Using Median Blur technique
+    median = cv2.medianBlur(res, 15)
+    # Using Bilateral Filter
+    bilateral = cv2.bilateralFilter(res, 15, 75, 75)    
+    
+    
+    cv2.imshow('frame', frame)
+    #cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+    #cv2.imshow('smoothed', smoothed)
+    #cv2.imshow('blur', blur)
+    #cv2.imshow('median', median)
+    #cv2.imshow('bilateral', bilateral)
+    
+    k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
     
