0e16ae3a7908a9d46e3895d03240395ec9a14e5f
SanchitGupta001 <sanchit.gupta011@gmail.com>
2016-11-02 02:59:49 +0530

    Added Extra_Python_Scripts folder and a python file

---
+++ Extra_Python_Scripts/Downloading_list_of_images_from_a_url.py
@@ -0,0 +1,46 @@
+import urllib.request
+import cv2
+import numpy as np
+import os
+
+def store_raw_images():# (Change it accordingly)
+    neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'   
+    neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
+    pic_num = 1
+    
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
+        
+    for i in neg_image_urls.split('\n'):
+        try:
+            print(i)
+            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
+            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            # should be larger than samples / pos pic (so we can place our image on it)
+            resized_image = cv2.resize(img, (100, 100))
+            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
+            pic_num += 1
+            
+        except Exception as e:
+            print(str(e))  
+            
+            
+#store_raw_images()   
+
+# For removing unwanted files(Change it accordingly)
+
+def find_uglies():
+    match = False
+    for file_type in ['neg']:
+        for img in os.listdir(file_type):
+            for ugly in os.listdir('uglies'):
+                try:
+                    current_image_path = str(file_type)+'/'+str(img)
+                    ugly = cv2.imread('uglies/'+str(ugly))
+                    question = cv2.imread(current_image_path)
+                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+                        print('That is one ugly pic! Deleting!')
+                        print(current_image_path)
+                        os.remove(current_image_path)
+                except Exception as e:
+                    print(str(e))         
